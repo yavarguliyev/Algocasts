5194baa17b8ae86c0b9125ca63a92e62dc2c99b8
yavarguliyev <yavarrg@code.edu.az>
2022-03-24 14:58:24 +0400

    weave

--- complete_exercises/weave/queue.js
+++ complete_exercises/weave/queue.js
@@ -1,8 +1,14 @@
-// --- Directions
-// Implement a 'peek' method in this Queue class.
-// Peek should return the last element (the next
-// one to be returned) from the queue *without*
-// removing it.
+//#region description
+
+/*
+  --- Directions
+  Implement a 'peek' method in this Queue class.
+  Peek should return the last element (the next
+  one to be returned) from the queue *without*
+  removing it.
+ */
+
+//#endregion
 
 class Queue {
   constructor() {
