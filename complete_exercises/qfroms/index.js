76986c9abf8c7a8b605661f3f4151b14167d5179
yavarguliyev <yavarrg@code.edu.az>
2022-03-24 15:51:54 +0400

    qfroms

--- complete_exercises/qfroms/index.js
+++ complete_exercises/qfroms/index.js
@@ -1,16 +1,26 @@
-// --- Directions
-// Implement a Queue datastructure using two stacks.
-// *Do not* create an array inside of the 'Queue' class.
-// Queue should implement the methods 'add', 'remove', and 'peek'.
-// For a reminder on what each method does, look back
-// at the Queue exercise.
-// --- Examples
-//     const q = new Queue();
-//     q.add(1);
-//     q.add(2);
-//     q.peek();  // returns 1
-//     q.remove(); // returns 1
-//     q.remove(); // returns 2
+//#region description
+
+/*
+  --- Directions
+  Implement a Queue datastructure using two stacks.
+  *Do not* create an array inside of the 'Queue' class.
+  Queue should implement the methods 'add', 'remove', and 'peek'.
+  For a reminder on what each method does, look back
+  at the Queue exercise.
+  --- Examples
+     const q = new Queue();
+     q.add(1);
+     q.add(2);
+     q.peek();  // returns 1
+     q.remove(); // returns 1
+     q.remove(); // returns 2
+ */
+
+//#endregion
+
+//#region solutions
+
+// @desc: solution one
 
 const Stack = require('./stack');
 
@@ -53,4 +63,8 @@ class Queue {
   }
 }
 
+//#endregion
+
 module.exports = Queue;
+
+
