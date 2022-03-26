e4c5cbbc5a5d21c4c1a0b71f92aa0c2e08a39dee
yavarguliyev <yavarrg@code.edu.az>
2022-03-26 14:27:12 +0400

    events

--- complete_exercises/events/index.js
+++ complete_exercises/events/index.js
@@ -14,16 +14,35 @@
 // @desc: solution one
 
 class Events {
+  constructor() {
+    this.events = {};
+  }
+
   // Register an event handler
-  on(eventName, callback) { }
+  on(eventName, callback) {
+    if (this.events[eventName]) {
+      this.events[eventName].push(callback);
+    } else {
+      this.events[eventName] = [callback];
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) { }
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      for (let cb of this.events[eventName]) {
+        cb();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) { }
+  off(eventName) {
+    // this.events[eventName] = [];
+    delete this.events[eventName];
+  }
 }
 
 //#endregion
