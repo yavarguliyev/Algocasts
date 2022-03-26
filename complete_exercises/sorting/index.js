76244f37550e72462f75cfd2f8cc2b27afb6a0fc
yavarguliyev <yavarrg@code.edu.az>
2022-03-26 15:47:57 +0400

    mergeSort

--- complete_exercises/sorting/index.js
+++ complete_exercises/sorting/index.js
@@ -49,7 +49,17 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) { }
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
 
 function merge(left, right) {
   const results = [];
