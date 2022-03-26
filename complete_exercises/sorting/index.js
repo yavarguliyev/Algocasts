e75b124fcb9c13638a9afc840af8846e2d30fb2e
yavarguliyev <yavarrg@code.edu.az>
2022-03-26 15:15:47 +0400

    selection

--- complete_exercises/sorting/index.js
+++ complete_exercises/sorting/index.js
@@ -12,10 +12,41 @@
 // @desc: solution one
 
 function bubbleSort(arr) {
+  // implement bubbleSort
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
 
+  // return sorted array
+  return arr;
 }
 
 function selectionSort(arr) {
+  // implement selectionSort
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+
+  // return sorted array
+  return arr;
 
 }
 
