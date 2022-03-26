5e655c93185959bf223c515e62ca3e47b8e1a19d
yavarguliyev <yavarrg@code.edu.az>
2022-03-26 13:48:11 +0400

    bst

--- complete_exercises/bst/index.js
+++ complete_exercises/bst/index.js
@@ -21,7 +21,42 @@
 
 // @desc: solution one
 
-class Node { }
+// @desc: Node.contains()
+// @desc: [https://developer.mozilla.org/en-US/docs/Web/API/Node/contains]
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+
+  contains(data) {
+    if (this.data === data) {
+      return this;
+    }
+
+    if (this.data < data && this.right) {
+      return this.right.contains(data);
+    } else if (this.data > data && this.left) {
+      return this.left.contains(data);
+    }
+
+    return null;
+  }
+}
 
 //#endregion
 
